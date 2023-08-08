139a48607fd474ee3dd8c20b562c712573db1a07
= <loeizguallar@gmail.com>
2023-08-08 23:58:09 +0200

    fix(client): wrong level calculation from xp

--- client/src/api/structures/User.js
+++ client/src/api/structures/User.js
@@ -22,8 +22,16 @@ export default class User {
 
     get XPUntilNextLevel() {
         const XPTiers = [200, 350, 500, 650, 750, 850, 950, 1050, 1150, 1250,
-        1350, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 2000];
-        var level = XPTiers.reduce((prev, curr) => prev += this.xp > curr ? 1 : 0, 0);
+            1350, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 2000];
+        var value = XPTiers.slice(0).reduce((prev, curr, _, arr) => {
+            if(xp < prev + curr) arr.splice(1);
+            prev += curr;
+            return curr
+        }, 0);
+        var level = XPTiers.indexOf(value);
+        if (xp > 2000) {
+            level += Math.floor((xp - 2000) / 2000);
+        }
         return level === 22 ? 2000 - this.xp : XPTiers[level] - this.xp;
     }
 
