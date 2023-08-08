b0c03220243d62510318ecebd8e5bf17e77cfdcf
= <loeizguallar@gmail.com>
2023-08-08 22:53:39 +0200

    fix(client): wrong xp bar text details

--- client/src/interfaces/home/components/XPBarComponent.js
+++ client/src/interfaces/home/components/XPBarComponent.js
@@ -18,12 +18,12 @@ export default class XPBarComponent extends Component {
         filledBar.id = 'filled-bar';
 
         const XPTiers = [200, 350, 500, 650, 750, 850, 950, 1050, 1150, 1250,
-        1350, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900];
+        1350, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 2000];
 
         filledBar.style.width = 100 - (100 * this.user.XPUntilNextLevel) / (XPTiers[this.user.level - 1] || 2000) + '%';
 
         const details = document.createElement('span');
-        details.textContent = `${(XPTiers[this.user.level - 1] || 2000) - this.user.XPUntilNextLevel} / ${XPTiers[this.user.level - 1] || 2000} XP`;
+        details.textContent = `${(XPTiers[this.user.level] || 2000) - this.user.XPUntilNextLevel} / ${XPTiers[this.user.level] || 2000} XP`;
 
         bar.append(filledBar);
         this.element.append(bar, details)
